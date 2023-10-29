51a23955516d27ff586b53b8f50676fd37d10607
suhas <hi@suhas.one>
2023-10-29 12:13:59 -0500

    initial comit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		GPTutor
+	</h1>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: flex-start;
+		flex: 0.5;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
